7ba7d2454c6cbdeb49f67fcd0bb7555dd418be54
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-02 19:49:56 +0530

    setting up

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import axios from "axios";
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getData = async () => {
-    const res = await axios.get("https://rickandmortyapi.com/api/character");
-    setUsers(res.data.results);
+    try {
+      const res = await axios.get("https://rickandmortyapi.com/api/character");
+      setUsers(res.data.results);
+    } catch (err) {
+      console.error("trouble fetching data", err);
+      return [];
+    }
   };
 
   useEffect(() => {
@@ -27,39 +33,48 @@ const App = () => {
         alignItems: "center",
       }}
     >
-      <table
-        style={{
-          border: "1px solid black",
-          width: "50rem",
-          marginTop: "2rem",
-          height: "80vh",
-        }}
-      >
-        <thead style={{ backgroundColor: "lightgray" }}>
-          <tr>
-            <td style={cellStyle}>ID</td>
-            <td style={cellStyle}>Name</td>
-            <td style={cellStyle}>Status</td>
-            <td style={cellStyle}>Gender</td>
-            <td style={cellStyle}>Image</td>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => {
-            return (
-              <tr key={index}>
-                <td style={cellStyle}>{user.id}</td>
-                <td style={cellStyle}>{user.name}</td>
-                <td style={cellStyle}>{user.status}</td>
-                <td style={cellStyle}>{user.gender}</td>
-                <td style={cellStyle} >
-                  <img style={{objectFit:"contain"}} width="80px" src={user.image}></img>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table
+          style={{
+            border: "1px solid black",
+            width: "50rem",
+            marginTop: "2rem",
+            height: "80vh",
+            borderCollapse: "collapse",
+          }}
+        >
+          <thead style={{ backgroundColor: "lightgray" }}>
+            <tr>
+              <td style={cellStyle}>ID</td>
+              <td style={cellStyle}>Name</td>
+              <td style={cellStyle}>Status</td>
+              <td style={cellStyle}>Gender</td>
+              <td style={cellStyle}>Image</td>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td style={cellStyle}>{user.id}</td>
+                  <td style={cellStyle}>{user.name}</td>
+                  <td style={cellStyle}>{user.status}</td>
+                  <td style={cellStyle}>{user.gender}</td>
+                  <td style={cellStyle}>
+                    <img
+                      style={{ objectFit: "contain" }}
+                      width="80px"
+                      src={user.image}
+                    ></img>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
