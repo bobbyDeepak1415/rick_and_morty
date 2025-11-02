ee8caf0906429a44d4cdadb5cfd99f91f6a3f547
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-03 01:34:41 +0530

    setting up

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,10 +3,23 @@ import React, { useEffect, useState } from "react";
 
 const Demo = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error,setError]=useState("")
+  const [filter,setFilter]=useState("All")
 
   const getData = async () => {
-    const res = await axios.get("https://rickandmortyapi.com/api/character");
-    setUsers(res.data.results);
+    try{
+      setLoading(true)
+
+      const res = await axios.get("https://rickandmortyapi.com/api/character");
+      setUsers(res.data.results);
+    }catch(err){
+      setError("trouble fetching data",err)
+
+
+    }finally{
+      setLoading(false)
+    }
   };
 
   useEffect(() => {
@@ -27,43 +40,61 @@ const Demo = () => {
         alignItems: "center",
       }}
     >
-      <table
-        style={{
-          marginTop: "2rem",
-          border: "1px solid black",
-          width: "70vw",
-          height: "70vh",
-          textAlign: "left",
-        }}
-      >
-        <thead>
-          <tr>
-            <td style={cellStyle}>ID</td>
-            <td style={cellStyle}>Name</td>
-            <td style={cellStyle}>Status</td>
-            <td style={cellStyle}>Gender</td>
-            <td style={cellStyle}>Image</td>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => {
-            return (
-              <tr>
-                <td style={cellStyle}>{user.id}</td>
-                <td style={cellStyle}>{user.name}</td>
-                <td style={cellStyle}>{user.status}</td>
-                <td style={cellStyle}>{user.gender}</td>
-                <td style={cellStyle}>
-                  <img
-                    style={{ height: "70px", width: "70px",objectFit:"contain", }}
-                    src={user.image}
-                  ></img>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+
+      <select value={filter} onChange={(e)=>setFilter(e.target.value)}>
+        <option>All</option>
+        <option>Alive</option>
+        <option>Dead</option>
+        <option>unknown</option>
+      </select>
+
+{error && (<p>{error}</p>)}
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table
+          style={{
+            marginTop: "2rem",
+            border: "1px solid black",
+            width: "70vw",
+            height: "70vh",
+            textAlign: "left",
+          }}
+        >
+          <thead>
+            <tr>
+              <td style={cellStyle}>ID</td>
+              <td style={cellStyle}>Name</td>
+              <td style={cellStyle}>Status</td>
+              <td style={cellStyle}>Gender</td>
+              <td style={cellStyle}>Image</td>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr>
+                  <td style={cellStyle}>{user.id}</td>
+                  <td style={cellStyle}>{user.name}</td>
+                  <td style={cellStyle}>{user.status}</td>
+                  <td style={cellStyle}>{user.gender}</td>
+                  <td style={cellStyle}>
+                    <img
+                      style={{
+                        height: "70px",
+                        width: "70px",
+                        objectFit: "contain",
+                      }}
+                      src={user.image}
+                    ></img>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
