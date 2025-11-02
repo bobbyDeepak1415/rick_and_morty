f2a97d3fdd30b543ead6a78c78a1a200e4ad76b2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-02 23:05:12 +0530

    setting up

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,44 +3,77 @@ import React, { useEffect, useState } from "react";
 
 const Demo = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const getData = async () => {
-    const res = await axios.get("https://rickandmortyapi.com/api/character");
-    setUsers(res.data.results);
+    try {
+      setLoading(true);
+      const res = await axios.get("https://rickandmortyapi.com/api/character");
+      setUsers(res.data.results);
+    } catch (err) {
+      setError("trouble fetching data", err);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     getData();
   }, []);
 
+  const cellStyle = {
+    border: "1px solid black",
+    padding: "1rem",
+  };
+
   return (
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-      <table style={{width:"50rem",border:"1px solid black",height:"80vh",marginTop:"1rem"}}>
-        <thead>
-          <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>Status</td>
-            <td>Gender</td>
-            <td>Image</td>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => {
-            return (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td>{user.status}</td>
-                <td>{user.gender}</td>
-                <td>
-                  <img src={user.image}></img>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {error && <p>{error}</p>}
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table
+          style={{
+            width: "50rem",
+            border: "1px solid black",
+            height: "80vh",
+            marginTop: "1rem",
+          }}
+        >
+          <thead>
+            <tr>
+              <td style={cellStyle}> ID</td>
+              <td style={cellStyle}>Name</td>
+              <td style={cellStyle}>Status</td>
+              <td style={cellStyle}>Gender</td>
+              <td style={cellStyle}>Image</td>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td style={cellStyle}>{user.id}</td>
+                  <td style={cellStyle}>{user.name}</td>
+                  <td style={cellStyle}>{user.status}</td>
+                  <td style={cellStyle}>{user.gender}</td>
+                  <td style={cellStyle}>
+                    <img style={{ height: "60px" }} src={user.image}></img>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
