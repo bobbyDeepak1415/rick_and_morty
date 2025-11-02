1f674726114e853de0b09c69283ade5246b21e0d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-03 01:08:11 +0530

    setting up

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,95 +1,48 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Demo = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [filter, setFilter] = useState("All");
-
-  const getData = async () => {
-    try {
-      setLoading(true);
-      const res = await axios.get("https://rickandmortyapi.com/api/character");
-      setUsers(res.data.results);
-    } catch (err) {
-      setError("trouble fetching data", err);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const lights = ["red", "orange", "green"];
+  const [currentLight, setCurrentLight] = useState(0);
 
   useEffect(() => {
-    getData();
+    const interval = setInterval(() => {
+      setCurrentLight((prev) => (prev + 1) % lights.length);
+    }, 1200);
+
+    return () => clearInterval(interval);
   }, []);
 
-  const cellStyle = {
-    border: "1px solid black",
-    padding: "1rem",
+  const getColour = (index) => {
+    return currentLight === index ? lights[index] : "gray";
   };
 
-  const filteredUsers =
-    filter === "All" ? users : users.filter((user) => user.status === filter);
-
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      {error && <p>{error}</p>}
-
-      <select
-        style={{ padding: "0.5rem" }}
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)}
+    <div style={{ justifyContent: "center", display: "flex" }}>
+      <div
+        style={{
+          backgroundColor: "black",
+          height: "25rem",
+          width: "12rem",
+          justifyContent: "center",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
       >
-        <option value="All">All</option>
-        <option value="Alive">Alive</option>
-        <option value="Dead">Dead</option>
-        <option value="unknown">unKnown</option>
-      </select>
-
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <table
-          style={{
-            width: "50rem",
-            border: "1px solid black",
-            height: "80vh",
-            marginTop: "1rem",
-          }}
-        >
-          <thead>
-            <tr>
-              <td style={cellStyle}> ID</td>
-              <td style={cellStyle}>Name</td>
-              <td style={cellStyle}>Status</td>
-              <td style={cellStyle}>Gender</td>
-              <td style={cellStyle}>Image</td>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredUsers.map((user) => {
-              return (
-                <tr key={user.id}>
-                  <td style={cellStyle}>{user.id}</td>
-                  <td style={cellStyle}>{user.name}</td>
-                  <td style={cellStyle}>{user.status}</td>
-                  <td style={cellStyle}>{user.gender}</td>
-                  <td style={cellStyle}>
-                    <img style={{ height: "60px" }} src={user.image}></img>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      )}
+        {lights.map((light, index) => {
+          return (
+            <div
+              style={{
+                height: "5rem",
+                width: "5rem",
+                backgroundColor: getColour(index),
+                margin: "auto",
+                borderRadius: "50%",
+              }}
+            ></div>
+          );
+        })}
+      </div>
     </div>
   );
 };
