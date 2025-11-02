23a4ba653574c2e93be196038036a9768d3e915d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-02 22:58:42 +0530

    setting up

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Demo = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [users, setUsers] = useState([]);
 
-export default Demo
+  const getData = async () => {
+    const res = await axios.get("https://rickandmortyapi.com/api/character");
+    setUsers(res.data.results);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <table style={{width:"50rem",border:"1px solid black",height:"80vh",marginTop:"1rem"}}>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Gender</td>
+            <td>Image</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.status}</td>
+                <td>{user.gender}</td>
+                <td>
+                  <img src={user.image}></img>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Demo;
