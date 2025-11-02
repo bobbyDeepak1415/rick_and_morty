8b6b4e1fca6a7d5d69384d13062f15790e498f1f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-02 18:54:40 +0530

    setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,39 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function App() {
+import axios from "axios";
+
+const App = () => {
   const [users, setUsers] = useState([]);
 
   const getData = async () => {
-    try {
-      const res = await axios.get("https://rickandmortyapi.com/api/character");
-      setUsers(res.data.results);
-    } catch (error) {
-      console.error("trouble fetching data", error);
-      return [];
-    }
+    const res = await axios.get("https://rickandmortyapi.com/api/character");
+    setUsers(res.data.results);
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  const cellStyle = {
-    padding: "1rem",
-    border: "1px solid black",
-  };
-
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        marginTop: "2rem",
-      }}
-    >
-      <table
-        style={{
-          width: "40rem",
-          borderCollapse: "collapse",
-          textAlign: "left",
-          border: "1px solid black",
-        }}
-      >
-        <thead style={{ backgroundColor: "#f2f2f2" }}>
+    <div>
+      <table>
+        <thead>
           <tr>
-            <td style={cellStyle}>Sno.</td>
-            <td style={cellStyle}>name</td>
-            <td style={cellStyle}>status</td>
-            <td style={cellStyle}>species</td>
-            <td style={cellStyle}>gender</td>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Species</td>
+            <td>Gender</td>
           </tr>
         </thead>
         <tbody>
-          {users.map((user, index) => (
-            <tr key={index} style={{ borderBottom: "1px solid #ddd" }}>
-              <td style={cellStyle}>{user.id}</td>
-              <td style={cellStyle}>{user.name}</td>
-              <td style={cellStyle}>{user.status}</td>
-              <td style={cellStyle}>{user.species}</td>
-              <td style={cellStyle}>{user.gender}</td>
-            </tr>
-          ))}
+          {users.map((user, index) => {
+            return <li key={index}>{user.name}</li>;
+          })}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
 export default App;
-
-
-
