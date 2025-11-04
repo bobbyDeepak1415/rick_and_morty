0938e46a3f1bffa3f87e70a0b2010aa1e24861c0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-04 16:04:05 +0530

    setting up

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,29 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-// import Demo from './Demo.jsx'
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// // import App from "./App.jsx";
+// import Demo from "./Demo.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-    {/* <Demo/> */}
-  </StrictMode>
-);
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     {/* <App /> */}
+//     <Demo />
+//   </StrictMode>
+// );
 
+let arr1 = [0, 3, 3, 3, 4, 5, 4, 6, 6, 6];
+
+
+function display(arr1) {
+  let k = 1;
+  arr1.sort()
+  for (let i =1; i < arr1.length; i++) {
+    if (arr1[i] !== arr1[i - 1]) {
+      arr1[i]+=1
+      k++;
+    }
+  }
+
+  return [k,arr1]
+}
+
+console.log(display(arr1));
