b251972ef56a0e80084f1a28bfecd0a492dac576
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-29 19:11:19 +0530

    setup done

--- src/Demo3.jsx
+++ src/Demo3.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 const Demo3 = () => {
   const [page, setPage] = useState(1);
 
+  const [onSubmit,setOnSubmit]=useState(false)
+
   const [userDetails, setUserDetails] = useState({
     name: "",
     email: "",
@@ -13,6 +15,8 @@ const Demo3 = () => {
   const isPage1Valid = userDetails.name && userDetails.email;
   const isPage2Valid = userDetails.role && userDetails.terms;
 
+
+  
   return (
     <div>
       {page === 1 && (
@@ -25,13 +29,41 @@ const Demo3 = () => {
             }
           ></input>
           <input
-          type="email"
+            type="email"
             value={userDetails.email}
             onChange={(e) =>
               setUserDetails({ ...userDetails, email: e.target.value })
             }
           ></input>
-          <button>Enter</button>
+          <button disabled={!isPage1Valid} onClick={() => setPage(2)}>
+            Enter
+          </button>
+        </div>
+      )}
+      {page === 2 && (
+        <div>
+          <h1>Select Your Role:</h1>
+          <select
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, role: e.target.value })
+            }
+          >
+            <option>Select role</option>
+            <option value="plumber">plumber</option>
+            <option value="fighter">fighter</option>
+            <option value="lawyer">lawyer</option>
+          </select>
+          <input
+            type="checkbox"
+            checked={true}
+            // *
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, terms: e.target.checked })
+            }
+          ></input>
+          <button disabled={!isPage2Valid} onClick={()=>onSubmit(true)}>
+            Enter
+          </button>
         </div>
       )}
     </div>
