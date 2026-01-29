e54ff8e56e1633d301145c785e304fefec717c48
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-29 16:30:47 +0530

    msg

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import App from "./App.jsx";
 // import Demo from "./Demo.jsx";
-import Demo2 from "./Demo2.jsx";
+// import Demo2 from "./Demo2.jsx";
+import Demo3 from "./Demo3.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Demo2 />
+    <Demo3 />
   </StrictMode>
 );
