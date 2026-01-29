a43f26f28aba7dcbf93322f5b74c0994bf2b96d5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-30 00:17:02 +0530

    setup done

--- src/Demo3.jsx
+++ src/Demo3.jsx
@@ -13,7 +13,61 @@ const Demo3 = () => {
   const isPage1Valid = userDetails.name && userDetails.email;
   const isPage2Valid = userDetails.role && userDetails.terms;
 
-  return <div></div>;
+  return (
+    <div style={{ height: "100vh", backgroundColor: "gray" }}>
+      {page === 1 && (
+        <div>
+          <h1>Enter your details here:</h1>
+
+          <input
+            value={userDetails.name}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, name: e.target.value })
+            }
+          />
+
+          <input
+            value={userDetails.email}
+            type="email"
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, email: e.target.value })
+            }
+          />
+          <button disabled={!isPage1Valid} onClick={() => setPage(2)}>
+            Enter
+          </button>
+        </div>
+      )}
+      {page === 2 && (
+        <div>
+          <h1>Enter your details here:</h1>
+
+          <select
+            value={userDetails.role}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, role: e.target.value })
+            }
+          >
+            <option>Select Role</option>
+            <option value="developer">Developer</option>
+            <option value="manager">Manager</option>
+            <option value="tester">Tester</option>
+          </select>
+
+          <input
+          type="checkbox"
+            checked={userDetails.terms}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, email: e.target.checked })
+            }
+          />
+          <button disabled={!isPage1Valid} onClick={() => setPage(2)}>
+            Enter
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Demo3;
