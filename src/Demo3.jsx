c9dfe71acd5973bd401d83f499dfd06d48f7ec3b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-29 17:03:39 +0530

    setup done

--- src/Demo3.jsx
+++ src/Demo3.jsx
@@ -13,7 +13,34 @@ const Demo3 = () => {
   const isStep1Valid = formData.name && formData.email;
   const isStep2Valid = formData.role && formData.terms;
 
-  return <div></div>;
-};...Demo3.apply.
+  return (
+    <div>
+      {step ===
+        1(
+          <>
+            <h2>Step1:Basic Information</h2>
+
+            <input
+              value={formData.name}
+              placeholder="full name"
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+            ></input>
+            <input
+              type="email"
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              value={formData.email}
+            ></input>
+            <button disabled={!isStep1Valid} onClick={() => setStep(2)}>
+              Enter
+            </button>
+          </>,
+        )}
+    </div>
+  );
+};
 
 export default Demo3;
