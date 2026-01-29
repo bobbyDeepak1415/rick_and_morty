ee8df0d506edebfbd288c597c967bb890f75c292
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-29 19:03:04 +0530

    setup done

--- src/Demo3.jsx
+++ src/Demo3.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Demo3 = () => {
+  const [page, setPage] = useState(1);
+
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    role: "",
+    terms: false,
+  });
+
+  const isPage1Valid = userDetails.name && userDetails.email;
+  const isPage2Valid = userDetails.role && userDetails.terms;
+
   return <div></div>;
 };
 
