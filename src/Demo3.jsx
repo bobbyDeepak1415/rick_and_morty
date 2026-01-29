2033e292bb5c3f293962cd64c148d3e4f1cdb547
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-29 19:05:22 +0530

    setup done

--- src/Demo3.jsx
+++ src/Demo3.jsx
@@ -13,7 +13,29 @@ const Demo3 = () => {
   const isPage1Valid = userDetails.name && userDetails.email;
   const isPage2Valid = userDetails.role && userDetails.terms;
 
-  return <div></div>;
+  return (
+    <div>
+      {page === 1 && (
+        <div>
+          <h1>Enter your Details:</h1>
+          <input
+            value={userDetails.name}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, name: e.target.value })
+            }
+          ></input>
+          <input
+          type="email"
+            value={userDetails.email}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, email: e.target.value })
+            }
+          ></input>
+          <button>Enter</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Demo3;
