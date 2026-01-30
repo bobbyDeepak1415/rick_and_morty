07a0898630651fe570a1023cf446e174694ea9aa
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-31 00:31:07 +0530

    setup done

--- src/Demo2.jsx
+++ src/Demo2.jsx
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from "react";
 
 const Demo2 = () => {
+  const [users, setUsers] = useState([]);
+  const [statuses, setStatuses] = useState("All");
 
-  const [users,setUsers]=useState([])
-  const [statuses,setStatuses]=useState("All")
+  const [filter, setFilter] = useState("");
 
-  useEffect(()=>{
-    const fetchData=()=>{
-      
+  useEffect(() => {
+    const fetchData = async () => {
       const res = await axios.get("https://rickandmortyapi.com/api/character");
 
-      const result=res.data.results
-      setUsers(result)
+      const result = res.data.results;
+      setUsers(result);
+
+      const uniqueStatuses = Array.from(
+        new Set(result.map((user) => user.status)),
+      );
+
+      setStatuses(["All", ...uniqueStatuses]);
+    };
+
+    fetchData();
+  });
+
+  const filteredUsers=
+
+  return <div style={{ height: "100vh", backgroundColor: "slategray" }}>
 
 
-
-
-    }
-  })
-
-
-
-  return (
-    <div style={{ height: "100vh", backgroundColor: "slategray" }}>Hello</div>
-  );
+  </div>;
 };
 
 export default Demo2;
