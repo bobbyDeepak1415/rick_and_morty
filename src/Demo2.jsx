8589c77e0553b59f9116f8d1108bae9d954c6063
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-30 23:37:51 +0530

    setup done

--- src/Demo2.jsx
+++ src/Demo2.jsx
@@ -1,29 +1,41 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Demo2 = () => {
+  const [users, setUsers] = useState([]);
 
-  const [users,setUsers]=useState([])
+  const [statuses, setStatuses] = useState([]);
 
-  useEffect(()=>{
-    const fetchUsers=()=>{
-      try{
-
-        const response = axios.get("https://rickandmortyapi.com/api/character");
-        const 
-      }catch(e){
-        console.log("failed To fetch...",e)
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(
+          "https://rickandmortyapi.com/api/character",
+        );
+        setUsers(response.data.results);
+        const uniqueStatuses = new Set(
+          Array.from(users.map((user) => user.status)),
+        );
+      } catch (e) {
+        console.log("failed To fetch...", e);
       }
+    };
+    fetchUsers();
+  }, []);
 
-    }
-  })
-
-  const url = "https://rickandmortyapi.com/api/character";
   return (
-    <div>
-      Hello
+    <div style={{ height: "100vh", backgroundColor: "slategray" }}>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              {user.name}:<span>{user.status}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Demo2
+export default Demo2;
