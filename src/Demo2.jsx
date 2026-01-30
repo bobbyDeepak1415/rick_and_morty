56d60fd9f37c0ce41640587df0ca6ad152d1585a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-30 23:31:03 +0530

    setup done

--- src/Demo2.jsx
+++ src/Demo2.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Demo2 = () => {
+
+  const [users,setUsers]=useState([])
+
+  useEffect(()=>{
+    const fetchUsers=()=>{
+      try{
+
+        const response = axios.get("https://rickandmortyapi.com/api/character");
+        const 
+      }catch(e){
+        console.log("failed To fetch...",e)
+      }
+
+    }
+  })
+
+  const url = "https://rickandmortyapi.com/api/character";
   return (
     <div>
       Hello
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import App from "./App.jsx";
-import Demo from "./Demo.jsx";
+// import Demo from "./Demo.jsx";
+import Demo2 from "./Demo2.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Demo />
+    <Demo2 />
   </StrictMode>
 );
