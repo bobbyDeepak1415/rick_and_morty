b1a65a89af0fb0e9acc3db5fe6a8b015e404a4a5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-31 00:16:05 +0530

    setup done

--- src/Demo2.jsx
+++ src/Demo2.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Demo2 = () => {
+
+  const [users,setUsers]=useState([])
+  const [statuses,setStatuses]=useState("All")
+
+  useEffect(()=>{
+    const fetchData=()=>{
+      
+      const res = await axios.get("https://rickandmortyapi.com/api/character");
+
+      const result=res.data.results
+      setUsers(result)
+
+
+
+
+    }
+  })
+
+
+
   return (
     <div style={{ height: "100vh", backgroundColor: "slategray" }}>Hello</div>
   );
